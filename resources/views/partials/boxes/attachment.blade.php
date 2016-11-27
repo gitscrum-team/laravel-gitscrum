3134254dfbeced0627f875a0e9f1eabe15437582
Renato Marinho <renato.marinho@s2move.com>
2016-11-27 21:59:52 +0000

    style for attachment box

--- resources/views/partials/boxes/attachment.blade.php
+++ resources/views/partials/boxes/attachment.blade.php
@@ -1,4 +1,4 @@
-<div class="">
+<div class="bx-attachment">
     <h6>{{_('General Attachments')}}</h6>
     <ul class="list-group" style="padding: 0">
         @each('partials.lists.attachments-min', $list, 'attachment', 'partials.lists.no-items')
