c0c7ff7d972debecddc9b4b39a3416a6428889eb
Renato Marinho <renato.marinho@s2move.com>
2016-11-27 13:49:52 +0000

    login screen updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,25 @@
-@section('title',  _('Login'))
+@section('title',  _('GitScrum Login'))
 
 @extends('layouts.master')
 
 @section('content')
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+
+<a href="https://github.com/renatomarinho/laravel-gitscrum"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
+
 <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-6 col-lg-offset-3">
         <div class="middle-box text-center loginscreen">
-            <div>
-                <h3>{{_('Welcome to')}} Git<strong>Scrum</strong></h3>
+                <h5 class="lead">{{_('Welcome to')}} <strong>GitScrum</strong></h5>
 
-                <br />
+                <a href="{{route('auth.github')}}" class="btn btn-hg btn-info">
+                        <i class="fa fa-github" aria-hidden="true"></i>&nbsp;&nbsp;Login with <strong>GitHub</strong></a>
 
-                <div class="row">
-                    <a href="{{route('auth.github')}}" class="btn btn-hg btn-info">
-                        <i class="fa fa-github" aria-hidden="true"></i>&nbsp;&nbsp;Login with <strong>Github</strong></a>
+                <div class="text-center">
+                    <a class="github-button" href="https://github.com/renatomarinho/laravel-gitscrum" data-icon="octicon-star" data-style="mega" data-count-href="/renatomarinho/laravel-gitscrum/stargazers" data-count-api="/repos/renatomarinho/laravel-gitscrum#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star renatomarinho/laravel-gitscrum on GitHub">Star</a>
                 </div>
 
-                <br />
-
-                <!--
-                <div class="hr-line-dashed"></div>
-
-                <form class="form-horizontal m-t" role="form" method="POST" action="{{route('auth.dologin')}}">
-                    {{ csrf_field() }}
-                    <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                        <input type="email" name="username" value="{{old('username')}}" class="form-control" placeholder="username" required autofocus>
-                        @if ($errors->has('username'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('username') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                    <div class="form-group{{ $errors->has('passwd') ? ' has-error' : '' }}">
-                        <input type="password" name="passwd" class="form-control" placeholder="Password" required>
-                        @if ($errors->has('passwd'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('passwd') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-
-                    <div class="row">
-                        <button type="submit" class="btn btn-primary btn-outline block full-width m-b">Login</button>
-                    </div>
-
-                    <a href="#"><small>Forgot password?</small></a>
-                    <p class="text-muted text-center"><small>Do not have an account?</small></p>
-
-                    <div class="row">
-                        <a class="btn btn-sm btn-white btn-block" href="">Create an account</a>
-                    </div>
-
-                </form>
-                -->
-                <p class="m-t"> <small>The GitScrum is licensed under the <a href="http://opensource.org/licenses/GPL-3.0" target="_blank">GPL v3 license</a>.</small> </p>
-            </div>
+                <p class="small">The GitScrum is licensed under the <a href="http://opensource.org/licenses/GPL-3.0" target="_blank">GPL v3 license</a></p>
         </div>
     </div>
 </div>
