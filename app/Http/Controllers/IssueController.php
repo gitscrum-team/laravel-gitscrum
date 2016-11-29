6702e8fc81bc70340270ac2fe8a166eb4f76f3a8
Renato Marinho <renato.marinho@s2move.com>
2016-11-29 22:39:54 +0000

    card sorted by position field

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -46,7 +46,7 @@ class IssueController extends Controller
             $issues = Auth::user()->issues;
         }
 
-        $issues = $issues->groupBy('config_status_id');
+        $issues = $issues->sortBy('position')->groupBy('config_status_id');
 
         $configStatus = configStatus::where('type', 'issue')
             ->orderby('position', 'ASC')->get();
@@ -208,11 +208,15 @@ class IssueController extends Controller
 
             return redirect()->back()->with('success', _('Updated successfully'));
         } else {
+
+            $position = 0;
+
             foreach(json_decode($request->json) as $id){
                 $issue = Issue::find($id);
                 $issue->config_status_id = $request->status_id;
                 $issue->closed_user_id = Auth::id();
                 $issue->closed_at = Carbon::now();
+                $issue->position = $position++;
                 $issue->save();
             }
             return true;
