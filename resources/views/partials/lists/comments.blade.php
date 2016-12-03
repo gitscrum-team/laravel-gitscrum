26ce98f1e0f693d64cec356ae0ccfa77e5af0fb2
Renato Marinho <renato.marinho@s2move.com>
2016-12-03 17:00:19 +0000

    some adjusts

--- resources/views/partials/lists/comments.blade.php
+++ resources/views/partials/lists/comments.blade.php
@@ -1,9 +1,9 @@
-<div class="feed-element">
+<div class="comment-element">
     <a href="{{route('user.profile', ['username'=>$comment->user->username])}}" class="pull-left">
         <img alt="{{$comment->user->username}}" class="img-rounded avatar-min" src="{{$comment->user->avatar}}">
     </a>
-    <div class="media-body ">
-        <small class="pull-right comment-dateforhumans">{{$comment->dateforhumans}}</small>
+    <div class="media-body">
+        <small class="pull-right dateforhumans">{{$comment->dateforhumans}}</small>
         <a href="{{route('user.profile', ['username'=>$comment->user->username])}}"><strong>{{$comment->user->username}}</strong></a> {{_('commented')}} <br>
         <small class="text-muted"></small>
         <div class="well mbn pbn">{!! nl2br(e($comment->comment)) !!}</div>
