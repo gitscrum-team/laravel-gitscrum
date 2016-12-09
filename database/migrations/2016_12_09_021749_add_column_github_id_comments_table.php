44cf9417dcba370ad7cddac327a7c970a37cc1f5
Renato Marinho <renato.marinho@s2move.com>
2016-12-09 02:28:21 +0000

    migration to add column github_id

---
+++ database/migrations/2016_12_09_021749_add_column_github_id_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnGithubIdCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->bigInteger('github_id')->nullable()->unique('github_id_UNIQUE')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropColumn('github_id');
+        });
+    }
+}
