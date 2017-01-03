a434c1177b58dbd6f08532a0a239614130db5cdb
Willian Mano <willianmanoaraujo@gmail.com>
2017-01-03 15:45:37 -0300

    Fixes issue #133 - Erro ao adicionar colaboradores com gitlab

--- app/Classes/Gitlab.php
+++ app/Classes/Gitlab.php
@@ -238,6 +238,7 @@ class Gitlab implements ProviderInterface
             if (isset($collaborator->id)) {
                 $data = [
                     'provider_id' => $collaborator->id,
+                    'provider' => 'gitlab',
                     'username' => $collaborator->username,
                     'name' => $collaborator->name,
                     'avatar' => $collaborator->avatar_url,
