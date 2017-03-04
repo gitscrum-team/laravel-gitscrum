e4622f80da7e7720cbfad728c6bb3addf3a8bc3c
Renato Marinho <renato.marinho@s2move.com>
2017-03-04 15:22:25 +0000

    partials/includes/breadcrumb-sprint

--- resources/views/partials/includes/breadcrumb-sprint.blade.php
+++ resources/views/partials/includes/breadcrumb-sprint.blade.php
@@ -1,4 +1,4 @@
 @if(isset($obj->slug))
-<a href="{{route('product_backlogs.show', ['slug'=>$obj->productBacklog->slug])}}">{{trans('Product Backlog')}}</a> &raquo;
-<a href="{{route('sprints.show', ['slug'=>$obj->slug])}}">{{trans('Sprint Backlog')}}</a> &raquo;
+<a href="{{route('product_backlogs.show', ['slug'=>$obj->productBacklog->slug])}}">{{trans('gitscrum.product-backlog')}}</a> &raquo;
+<a href="{{route('sprints.show', ['slug'=>$obj->slug])}}">{{trans('gitscrum.sprint-backlog')}}</a> &raquo;
 @endif
