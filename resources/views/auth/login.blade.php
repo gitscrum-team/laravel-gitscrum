e9e3460fe25f06d1bd4f27239079dd87e5079c1a
Renato Marinho <renato.marinho@s2move.com>
2017-06-18 13:53:08 +0100

    update repository url

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 
-<a href="https://github.com/renatomarinho/laravel-gitscrum"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
+<a href="https://github.com/GitScrum-Community/laravel-gitscrum"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
 
 <div class="row">
     <div class="col-lg-6 col-lg-offset-3">
@@ -21,10 +21,11 @@
                 </a>
 
                 <div class="text-center">
-                    <a class="github-button" href="https://github.com/renatomarinho/laravel-gitscrum" data-icon="octicon-star" data-style="mega" data-count-href="/renatomarinho/laravel-gitscrum/stargazers" data-count-api="/repos/renatomarinho/laravel-gitscrum#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star renatomarinho/laravel-gitscrum on GitHub">Star</a>
+                    <a class="github-button" href="https://github.com/GitScrum-Community/laravel-gitscrum" data-icon="octicon-star" data-style="mega" data-count-href="/renatomarinho/laravel-gitscrum/stargazers" data-count-api="/repos/renatomarinho/laravel-gitscrum#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star renatomarinho/laravel-gitscrum on GitHub">Star</a>
                 </div>
 
-                <p class="small">The GitScrum is licensed under the <a href="http://opensource.org/licenses/GPL-3.0" target="_blank">GPL v3 license</a></p>
+                <p class="small">The GitScrum is licensed under the
+                    <a href="https://github.com/GitScrum-Community/laravel-gitscrum/blob/master/LICENSE.md" target="_blank">MIT license</a></p>
         </div>
     </div>
 </div>
