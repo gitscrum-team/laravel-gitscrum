ed3d9d13d5c9092f259af7c434a7a6bedc03751c
Didik Tri Susanto <rezd14@gmail.com>
2017-06-19 05:57:27 +0700

    add closable flash notification

--- resources/views/errors/flash-message.blade.php
+++ resources/views/errors/flash-message.blade.php
@@ -1,6 +1,9 @@
 @if(Session::has('success'))
     <div class="col-lg-12">
         <div class="alert alert-success alert-errors">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
          {{ Session::get('success') }}
         </div>
     </div>
--- resources/views/errors/notification-message.blade.php
+++ resources/views/errors/notification-message.blade.php
@@ -1,6 +1,9 @@
 @if(isset($notification['message']))
     <div class="col-lg-12 {{$notification['class'] or ''}}">
         <div class="alert alert-{{$notification['alert']}} alert-errors">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
          {!! $notification['message'] !!}
         </div>
     </div>
