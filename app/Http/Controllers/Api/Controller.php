fe0cb7ac31c8069587ee4d512ef6fad1ef72c2d9
Renato Marinho <renato.marinho@s2move.com>
2017-07-25 21:53:15 +0100

    composer new packages / folder for API Controller

---
+++ app/Http/Controllers/Api/Controller.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Laravel GitScrum <https://github.com/GitScrum-Community/laravel-gitscrum>
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2017 Renato Marinho <renato.marinho@s2move.com>
+ */
+
+namespace GitScrum\Http\Controllers\Api;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use File;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
