cc29cf33c1c5fec09c5f8a8134f9cc52625a5549
Renato Marinho <renato.marinho@s2move.com>
2017-09-14 21:46:55 +0100

    Add Laravel Blade Minify

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \RenatoMarinho\LaravelBladeMinify\Middleware\Minify::class,
     ];
 
     /**
