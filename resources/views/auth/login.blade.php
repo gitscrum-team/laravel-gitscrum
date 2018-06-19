e8397be82c56a2899fb55930b973805f44486669
mahyar torkamanzadeh <mahyar.tz@gmail.com>
2018-06-19 10:59:10 +0430

    Changes On Login Page
    changes on Login Page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,6 @@
 @section('content')
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 
-<a href="https://github.com/GitScrum-Community/laravel-gitscrum"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/e7bbb0521b397edbd5fe43e7f760759336b5e05f/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677265656e5f3030373230302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png"></a>
 
     <div class="authentication__left-screen">
 
@@ -14,7 +13,6 @@
             <div class="text-center">
 
                 <div class="authentication__logo">
-                    <img src="{{ asset('img/gitscrum-circle.png') }}">
                 </div>
 
                 <h3 class="">
@@ -33,12 +31,6 @@
                 <a href="{{route('auth.provider', ['provider' => 'bitbucket'])}}" class="btn btn-lg btn-default btn-loader">
                     <i class="fa fa-bitbucket" aria-hidden="true"></i>&nbsp; <strong>Bitbucket</strong>
                 </a>
-
-                <a href="{{route('auth.provider', ['provider' => 'gitea'])}}" class="btn btn-lg btn-default btn-loader">
-                    <i class="fa" aria-hidden="true"></i>&nbsp; <strong>Gitea</strong>
-                </a>
-                <p class="small">The GitScrum Community is licensed under the
-                    <a href="https://github.com/gitscrum-community-edition/laravel-gitscrum/blob/master/LICENSE.md" target="_blank">MIT license</a></p>
 
             </div>
 
